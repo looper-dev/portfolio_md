c3b6405a88d47ed1eb3c5df03e890ca70e8353f8
looper-dev <113578080+looper-dev@users.noreply.github.com>
2022-09-28 17:11:03 +0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> © dev-suwonza123.github.io.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
